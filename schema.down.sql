ac86b3d0ac7e92a33de527620f70eb4b2468035c
PawelSkorupa <pawelskorupa01@gmail.com>
2024-10-10 19:30:08 +0200

    Update application to use dockerized postgres database instead of H2

---
+++ schema.down.sql
@@ -0,0 +1,360 @@
+
+    set client_min_messages = WARNING;
+
+    alter table if exists address_owner 
+       drop constraint if exists FK6qqrocb197cfxf5of7wc9cu5s;
+
+    alter table if exists address_owner 
+       drop constraint if exists FKtlveceg81q5bjp3h05wef8s8h;
+
+    alter table if exists CreditCard 
+       drop constraint if exists FKl2cxkcslw7r5abrhbtj0qjyqo;
+
+    alter table if exists CreditCard 
+       drop constraint if exists FKfky0y471d7n8cebc1oe28idci;
+
+    alter table if exists customer_creditcard 
+       drop constraint if exists FKbqtt4y8vkl6a0hk4ud19wci0g;
+
+    alter table if exists customer_creditcard 
+       drop constraint if exists FKfjq2xyx1jh83r4h1t7f18kxj;
+
+    drop table if exists Address cascade;
+
+    drop table if exists address_owner cascade;
+
+    drop table if exists Bank cascade;
+
+    drop table if exists CreditCard cascade;
+
+    drop table if exists Customer cascade;
+
+    drop table if exists customer_creditcard cascade;
+
+    drop table if exists Pincode cascade;
+
+    drop table if exists Todo cascade;
+
+    set client_min_messages = WARNING;
+
+    alter table if exists address_owner 
+       drop constraint if exists FK6qqrocb197cfxf5of7wc9cu5s;
+
+    alter table if exists address_owner 
+       drop constraint if exists FKtlveceg81q5bjp3h05wef8s8h;
+
+    alter table if exists CreditCard 
+       drop constraint if exists FKl2cxkcslw7r5abrhbtj0qjyqo;
+
+    alter table if exists CreditCard 
+       drop constraint if exists FKfky0y471d7n8cebc1oe28idci;
+
+    alter table if exists customer_creditcard 
+       drop constraint if exists FKbqtt4y8vkl6a0hk4ud19wci0g;
+
+    alter table if exists customer_creditcard 
+       drop constraint if exists FKfjq2xyx1jh83r4h1t7f18kxj;
+
+    drop table if exists Address cascade;
+
+    drop table if exists address_owner cascade;
+
+    drop table if exists Bank cascade;
+
+    drop table if exists CreditCard cascade;
+
+    drop table if exists Customer cascade;
+
+    drop table if exists customer_creditcard cascade;
+
+    drop table if exists Pincode cascade;
+
+    drop table if exists Todo cascade;
+
+    set client_min_messages = WARNING;
+
+    alter table if exists address_owner 
+       drop constraint if exists FK6qqrocb197cfxf5of7wc9cu5s;
+
+    alter table if exists address_owner 
+       drop constraint if exists FKtlveceg81q5bjp3h05wef8s8h;
+
+    alter table if exists CreditCard 
+       drop constraint if exists FKl2cxkcslw7r5abrhbtj0qjyqo;
+
+    alter table if exists CreditCard 
+       drop constraint if exists FKfky0y471d7n8cebc1oe28idci;
+
+    alter table if exists customer_creditcard 
+       drop constraint if exists FKbqtt4y8vkl6a0hk4ud19wci0g;
+
+    alter table if exists customer_creditcard 
+       drop constraint if exists FKfjq2xyx1jh83r4h1t7f18kxj;
+
+    drop table if exists Address cascade;
+
+    drop table if exists address_owner cascade;
+
+    drop table if exists Bank cascade;
+
+    drop table if exists CreditCard cascade;
+
+    drop table if exists Customer cascade;
+
+    drop table if exists customer_creditcard cascade;
+
+    drop table if exists Pincode cascade;
+
+    drop table if exists Todo cascade;
+
+    set client_min_messages = WARNING;
+
+    alter table if exists address_owner 
+       drop constraint if exists FK6qqrocb197cfxf5of7wc9cu5s;
+
+    alter table if exists address_owner 
+       drop constraint if exists FKtlveceg81q5bjp3h05wef8s8h;
+
+    alter table if exists CreditCard 
+       drop constraint if exists FKl2cxkcslw7r5abrhbtj0qjyqo;
+
+    alter table if exists CreditCard 
+       drop constraint if exists FKfky0y471d7n8cebc1oe28idci;
+
+    alter table if exists customer_creditcard 
+       drop constraint if exists FKbqtt4y8vkl6a0hk4ud19wci0g;
+
+    alter table if exists customer_creditcard 
+       drop constraint if exists FKfjq2xyx1jh83r4h1t7f18kxj;
+
+    drop table if exists Address cascade;
+
+    drop table if exists address_owner cascade;
+
+    drop table if exists Bank cascade;
+
+    drop table if exists CreditCard cascade;
+
+    drop table if exists Customer cascade;
+
+    drop table if exists customer_creditcard cascade;
+
+    drop table if exists Pincode cascade;
+
+    drop table if exists Todo cascade;
+
+    set client_min_messages = WARNING;
+
+    alter table if exists address_owner 
+       drop constraint if exists FK6qqrocb197cfxf5of7wc9cu5s;
+
+    alter table if exists address_owner 
+       drop constraint if exists FKtlveceg81q5bjp3h05wef8s8h;
+
+    alter table if exists CreditCard 
+       drop constraint if exists FKl2cxkcslw7r5abrhbtj0qjyqo;
+
+    alter table if exists CreditCard 
+       drop constraint if exists FKfky0y471d7n8cebc1oe28idci;
+
+    alter table if exists customer_creditcard 
+       drop constraint if exists FKbqtt4y8vkl6a0hk4ud19wci0g;
+
+    alter table if exists customer_creditcard 
+       drop constraint if exists FKfjq2xyx1jh83r4h1t7f18kxj;
+
+    drop table if exists Address cascade;
+
+    drop table if exists address_owner cascade;
+
+    drop table if exists Bank cascade;
+
+    drop table if exists CreditCard cascade;
+
+    drop table if exists Customer cascade;
+
+    drop table if exists customer_creditcard cascade;
+
+    drop table if exists Pincode cascade;
+
+    drop table if exists Todo cascade;
+
+    set client_min_messages = WARNING;
+
+    alter table if exists address_owner 
+       drop constraint if exists FK6qqrocb197cfxf5of7wc9cu5s;
+
+    alter table if exists address_owner 
+       drop constraint if exists FKtlveceg81q5bjp3h05wef8s8h;
+
+    alter table if exists CreditCard 
+       drop constraint if exists FKl2cxkcslw7r5abrhbtj0qjyqo;
+
+    alter table if exists CreditCard 
+       drop constraint if exists FKfky0y471d7n8cebc1oe28idci;
+
+    alter table if exists customer_creditcard 
+       drop constraint if exists FKbqtt4y8vkl6a0hk4ud19wci0g;
+
+    alter table if exists customer_creditcard 
+       drop constraint if exists FKfjq2xyx1jh83r4h1t7f18kxj;
+
+    drop table if exists Address cascade;
+
+    drop table if exists address_owner cascade;
+
+    drop table if exists Bank cascade;
+
+    drop table if exists CreditCard cascade;
+
+    drop table if exists Customer cascade;
+
+    drop table if exists customer_creditcard cascade;
+
+    drop table if exists Pincode cascade;
+
+    drop table if exists Todo cascade;
+
+    set client_min_messages = WARNING;
+
+    alter table if exists address_owner 
+       drop constraint if exists FK6qqrocb197cfxf5of7wc9cu5s;
+
+    alter table if exists address_owner 
+       drop constraint if exists FKtlveceg81q5bjp3h05wef8s8h;
+
+    alter table if exists CreditCard 
+       drop constraint if exists FKl2cxkcslw7r5abrhbtj0qjyqo;
+
+    alter table if exists CreditCard 
+       drop constraint if exists FKfky0y471d7n8cebc1oe28idci;
+
+    alter table if exists customer_creditcard 
+       drop constraint if exists FKbqtt4y8vkl6a0hk4ud19wci0g;
+
+    alter table if exists customer_creditcard 
+       drop constraint if exists FKfjq2xyx1jh83r4h1t7f18kxj;
+
+    drop table if exists Address cascade;
+
+    drop table if exists address_owner cascade;
+
+    drop table if exists Bank cascade;
+
+    drop table if exists CreditCard cascade;
+
+    drop table if exists Customer cascade;
+
+    drop table if exists customer_creditcard cascade;
+
+    drop table if exists Pincode cascade;
+
+    drop table if exists Todo cascade;
+
+    set client_min_messages = WARNING;
+
+    alter table if exists address_owner 
+       drop constraint if exists FK6qqrocb197cfxf5of7wc9cu5s;
+
+    alter table if exists address_owner 
+       drop constraint if exists FKtlveceg81q5bjp3h05wef8s8h;
+
+    alter table if exists CreditCard 
+       drop constraint if exists FKl2cxkcslw7r5abrhbtj0qjyqo;
+
+    alter table if exists CreditCard 
+       drop constraint if exists FKfky0y471d7n8cebc1oe28idci;
+
+    alter table if exists customer_creditcard 
+       drop constraint if exists FKbqtt4y8vkl6a0hk4ud19wci0g;
+
+    alter table if exists customer_creditcard 
+       drop constraint if exists FKfjq2xyx1jh83r4h1t7f18kxj;
+
+    drop table if exists Address cascade;
+
+    drop table if exists address_owner cascade;
+
+    drop table if exists Bank cascade;
+
+    drop table if exists CreditCard cascade;
+
+    drop table if exists Customer cascade;
+
+    drop table if exists customer_creditcard cascade;
+
+    drop table if exists Pincode cascade;
+
+    drop table if exists Todo cascade;
+
+    set client_min_messages = WARNING;
+
+    alter table if exists address_owner 
+       drop constraint if exists FK6qqrocb197cfxf5of7wc9cu5s;
+
+    alter table if exists address_owner 
+       drop constraint if exists FKtlveceg81q5bjp3h05wef8s8h;
+
+    alter table if exists CreditCard 
+       drop constraint if exists FKl2cxkcslw7r5abrhbtj0qjyqo;
+
+    alter table if exists CreditCard 
+       drop constraint if exists FKfky0y471d7n8cebc1oe28idci;
+
+    alter table if exists customer_creditcard 
+       drop constraint if exists FKbqtt4y8vkl6a0hk4ud19wci0g;
+
+    alter table if exists customer_creditcard 
+       drop constraint if exists FKfjq2xyx1jh83r4h1t7f18kxj;
+
+    drop table if exists Address cascade;
+
+    drop table if exists address_owner cascade;
+
+    drop table if exists Bank cascade;
+
+    drop table if exists CreditCard cascade;
+
+    drop table if exists Customer cascade;
+
+    drop table if exists customer_creditcard cascade;
+
+    drop table if exists Pincode cascade;
+
+    drop table if exists Todo cascade;
+
+    set client_min_messages = WARNING;
+
+    alter table if exists address_owner 
+       drop constraint if exists FK6qqrocb197cfxf5of7wc9cu5s;
+
+    alter table if exists address_owner 
+       drop constraint if exists FKtlveceg81q5bjp3h05wef8s8h;
+
+    alter table if exists CreditCard 
+       drop constraint if exists FKl2cxkcslw7r5abrhbtj0qjyqo;
+
+    alter table if exists CreditCard 
+       drop constraint if exists FKfky0y471d7n8cebc1oe28idci;
+
+    alter table if exists customer_creditcard 
+       drop constraint if exists FKbqtt4y8vkl6a0hk4ud19wci0g;
+
+    alter table if exists customer_creditcard 
+       drop constraint if exists FKfjq2xyx1jh83r4h1t7f18kxj;
+
+    drop table if exists Address cascade;
+
+    drop table if exists address_owner cascade;
+
+    drop table if exists Bank cascade;
+
+    drop table if exists CreditCard cascade;
+
+    drop table if exists Customer cascade;
+
+    drop table if exists customer_creditcard cascade;
+
+    drop table if exists Pincode cascade;
+
+    drop table if exists Todo cascade;
